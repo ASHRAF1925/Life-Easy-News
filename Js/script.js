9dcb3804e448ff646b375a3d783abf8ac9470db5
ASHRAF1925 <1925ashraf@gmail.com>
2022-09-03 22:42:21 +0600

    responsive  done

--- Js/script.js
+++ Js/script.js
@@ -1,32 +1,3 @@
-// category loading 
-
-const load_catergoris = () => {
-    const category_container = document.getElementById('category_selection');
-    const url = `https://openapi.programming-hero.com/api/news/categories`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => display_categoris(data.data.news_category))
-        .catch(error => console.log(error))
-
-
-
-}
-const display_categoris = (array) => {
-    for (var value of array) {
-        // console.log(value.category_name);
-        const new_item = document.createElement('div');
-        new_item.classList.add('text-center', 'px-4', 'text-center', 'my-auto', 'py-2', 'rounded', 'mx-2', 'Category_item');
-        new_item.innerHTML = `
-        ${value.category_name}
-        `
-        const categories = document.getElementById('category_selection');
-        categories.append(new_item);
-
-    }
-    category_selection();
-}
-load_catergoris();
-
 // categories selection
 const category_selection = () => {
     const categories = document.getElementById('category_selection');
@@ -49,4 +20,39 @@ const category_selection = () => {
     }
 
 }
-category_selection();
+
+// display categories
+const display_categoris = (array) => {
+    for (var value of array) {
+        // console.log(value.category_name);
+        const new_item = document.createElement('div');
+        new_item.classList.add('text-center', 'px-4', 'text-center', 'my-2', 'py-2', 'rounded', 'mx-2', 'Category_item');
+        new_item.innerHTML = `
+        ${value.category_name}
+        `
+        const categories = document.getElementById('category_selection');
+        categories.append(new_item);
+
+    }
+    category_selection();
+}
+
+
+// category loading 
+
+const load_catergoris = () => {
+    const category_container = document.getElementById('category_selection');
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => display_categoris(data.data.news_category))
+        .catch(error => console.log(error))
+
+
+
+}
+
+load_catergoris();
+
+
+
